2f527ba755be7a2bf860a63cb52dc44d2848d021
shrn82256 <shrn82256@gmail.com>
2019-03-04 22:35:18 +0530

    moved project to root folder

--- djangonautic/wsgi.py
+++ djangonautic/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangonautic.settings')
 
 application = get_wsgi_application()
+
+# from djangonautic.wsgi import DjangonauticApplication
+
+# application = DjangonauticApplication(application)
